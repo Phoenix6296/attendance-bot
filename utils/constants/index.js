88e34fbdd701b7eea9e28865b7fe3bb0805559d1
Krishna Biswakarma <krishna@openinapp.com>
2023-06-03 11:56:12 +0530

    Added the services page

---
+++ utils/constants/index.js
@@ -0,0 +1,8 @@
+export const VALID_URL_TEST =
+  /^(https?:\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\w \\.-]*)*\/?$/;
+
+export const DEFAULT_ERROR_FALLBACK = "Unable to Process, Please try again";
+export const ACCESS_TOKEN_KEY = "attendance_access_token";
+export const REFRESH_TOKEN_KEY = "attendance_refresh_token";
+export const ACTIVE_STATE =
+  "border border-[2px] border-primary  bg-primaryFade text-primary";
