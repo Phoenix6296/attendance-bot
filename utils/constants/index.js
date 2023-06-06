869d9c2508a3cc1a58b889452b2aea58b00625e9
Krishna Biswakarma <krishna@openinapp.com>
2023-06-06 10:55:34 +0530

    Updated the base URL

--- utils/constants/index.js
+++ utils/constants/index.js
@@ -1,9 +1,9 @@
 export const VALID_URL_TEST =
   /^(https?:\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\w \\.-]*)*\/?$/;
 
-export const BASE_URL = "https://93de-116-193-137-20.ngrok-free.app/api/v1";
+export const BASE_URL = "https://mitrajitvm.eastus.cloudapp.azure.com/api/v1";
 export const DEFAULT_ERROR_FALLBACK = "Unable to Process, Please try again";
 export const ACCESS_TOKEN_KEY = "attendance_access_token";
 export const REFRESH_TOKEN_KEY = "attendance_refresh_token";
 export const ACTIVE_STATE =
-  "border border-[2px] border-primary  bg-primaryFade text-primary";
+  "border border-[2px] border-primary bg-primaryFade text-primary";
